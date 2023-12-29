ffd5e9fbf80478c5fe903e88995ee9e076b0984a
Gromov Danil <105980448+Xenob1na@users.noreply.github.com>
2023-12-29 23:21:36 +0700

    Portfolio Page #2

--- pages/portfolio/index.vue
+++ pages/portfolio/index.vue
@@ -36,16 +36,69 @@ onBeforeMount(() => {
             src: "/clening.png",
             alt: "clening",
             Backend: "NodeJS(Express), Mysql",
-            Frontend: "NuxtJS(VueJS), TailwindCSS",
+            Frontend: "NuxtJS(VueJS), TS, TailwindCSS, Pinia",
+            link: 'https://cleaning-plum.vercel.app/'
         },
         {
             id: 2,
-            title: "Ocean Clening",
-            description: "Сайт клининговой компании, есть возможность формирования заказа, на определенную услугу, все данный попадают в админ панель и админ перевазванивает вам. P.S: На админке стоит пароль поэтому там секрет.",
-            src: "/clening.png",
-            alt: "clening",
-            Backend: "NodeJS(Express), Mysql",
-            Frontend: "NuxtJS(VueJS), TailwindCSS",
+            title: "CRUD Blog",
+            description: "CRUD опреации для блога",
+            src: "/crud.png",
+            alt: "crud",
+            Backend: "TS, Mysql",
+            Frontend: "NuxtJS(VueJS), TS, TailwindCSS",
+            link: 'https://github.com/Xenob1na/Nuxt-Mysql-CRUD'
+        },
+        {
+            id: 3,
+            title: "NLP",
+            description: "NLP - Landing page, для ознакомления и записи на онлайн курс по программированию, имеется возможность записать на курс, через модалку, данные отправляються в crm Email.js",
+            src: "/infoIT.png",
+            alt: "IT курсы",
+            Backend: "EmailJS",
+            Frontend: "VueJS, TailwindCSS, VueUse",
+            link: 'https://nlp-deploy.web.app/'
+        },
+        {
+            id: 4,
+            title: "Магнум",
+            description: "Магнум - компания занимающаяся транспортными перевозками по направлению ХМАО и ЯНАО.",
+            src: "/magnum.png",
+            alt: "Логистика",
+            Backend: "Не писался",
+            Frontend: "VueJS, TailwindCSS, VueUse",
+            PS: "Внимание: Является прототипом сайта, реального проекта",
+            link: 'https://xenob1na.github.io/Vue-Magnum/'
+        },
+        {
+            id: 5,
+            title: "Todo List",
+            description: "Todo List - приложение для заметок. Имеет возможность создания, удаления заметок. Также смена цветовой темы",
+            src: "/todo.png",
+            alt: "todo",
+            Backend: "Не писался",
+            Frontend: "VueJS, Scss",
+            link: 'https://xenob1na.github.io/Vue-ToDO/'
+        },
+        {
+            id: 6,
+            title: "Notes App",
+            description: "Notes App - приложение для заметок. Имеет возможность создания, редактирования, удаления заметок. Также реализована возможность поиска заметок. Приложение реализовано на VueJS",
+            src: "/NotesApp.png",
+            alt: "Заметки",
+            Backend: "Не писался",
+            Frontend: "VueJS, TailwindCSS, Pinia",
+            link: 'https://xenob1na.github.io/Notes-App/'
+        },
+        {
+            id: 7,
+            title: "Worku",
+            description: "Worku - пет проект, ознакомление с GulpJS",
+            src: "/worko.png",
+            alt: "Worku",
+            Backend: "Не писался",
+            Frontend: "GulpJS, Html, Scss, JavaScript",
+            link: 'https://xenob1na.github.io/Worku/dist/index.html'
         }
     ]
 })
